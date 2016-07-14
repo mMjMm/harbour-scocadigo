fcb31a00364422d5bc551436df1888b262d1efdc
Åke Engelbrektson <eson57@users.noreply.github.com>
2016-07-14 11:25:49 +0200

    Create harbour-scocadigo-sv.ts
    I have not played in many years, but an app would be helpful if I ever again... so here's Swedish translation if you want.

---
+++ translations/harbour-scocadigo-sv.ts
@@ -0,0 +1,364 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>About</name>
+    <message>
+        <source>about</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>SCOCADIGO is a scorecard app for Disc Golf. It is possible to add new players, courses and keep track of your Disc Golf rounds. Watch individual statistics and compare your scores with other players. Improve your game. Just play! Please be aware! you using an alpha version of scocadigo. There might be bugs! please report when you find one!</source>
+        <translation>SCOCADIGO är en poängkorts-app för Frisbeegolf. Du kan lägga till nya användare, banor och hålla koll på dina frisbeegolfrundor. Se individuell statistik och jämför dina resultat med andra spelare. Förbättra ditt spel. Bara spela! Kom ihåg att du använder en alfaversion av Scocadigo. Det kan finnas fel! Rapportera gärna om du hittar något!</translation>
+    </message>
+    <message>
+        <source>by</source>
+        <translation>av</translation>
+    </message>
+    <message>
+        <source>LICENSE: MIT </source>
+        <translation>LICENS: MIT </translation>
+    </message>
+</context>
+<context>
+    <name>Courses</name>
+    <message>
+        <source>courses</source>
+        <translation>banor</translation>
+    </message>
+</context>
+<context>
+    <name>CreatedScorecard</name>
+    <message>
+        <source>total </source>
+        <translation>totalt </translation>
+    </message>
+    <message>
+        <source>player</source>
+        <translation>spelare</translation>
+    </message>
+</context>
+<context>
+    <name>DefinePar</name>
+    <message>
+        <source>Hole: </source>
+        <translation>Hål: </translation>
+    </message>
+</context>
+<context>
+    <name>EditPlayers</name>
+    <message>
+        <source>SAVE CHANGES</source>
+        <translation>SPARA ÄNDRINGAR</translation>
+    </message>
+    <message>
+        <source>Nick</source>
+        <translation>Smeknamn</translation>
+    </message>
+    <message>
+        <source>First name</source>
+        <translation>Förnamn</translation>
+    </message>
+    <message>
+        <source>Last name</source>
+        <translation>Efternamn</translation>
+    </message>
+    <message>
+        <source>Email</source>
+        <translation>E-post</translation>
+    </message>
+    <message>
+        <source>PDGA Nr</source>
+        <translation>PDGA Nr</translation>
+    </message>
+    <message>
+        <source>Notes</source>
+        <translation>Anteckningar</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>score card disc golf</source>
+        <translation>poängkort frisbeegolf</translation>
+    </message>
+</context>
+<context>
+    <name>NewCourses</name>
+    <message>
+        <source>Course name:</source>
+        <translation>Bannamn:</translation>
+    </message>
+    <message>
+        <source>The course name already exists</source>
+        <translation>Bannamnet finns redan</translation>
+    </message>
+    <message>
+        <source>Number of baskets:</source>
+        <translation>Antal korgar:</translation>
+    </message>
+    <message>
+        <source>SAVE</source>
+        <translation>SPARA</translation>
+    </message>
+    <message>
+        <source>Baskets</source>
+        <translation>Korgar</translation>
+    </message>
+</context>
+<context>
+    <name>NewGame</name>
+    <message>
+        <source>New game</source>
+        <translation>Nytt spel</translation>
+    </message>
+    <message>
+        <source>select players</source>
+        <translation>välj spelare</translation>
+    </message>
+    <message>
+        <source>ADD PLAYER</source>
+        <translation>LÄGG TILL SPELARE</translation>
+    </message>
+    <message>
+        <source>No players</source>
+        <translation>Inga spelare</translation>
+    </message>
+    <message>
+        <source>pull down to add a new player</source>
+        <translation>dra neråt för att lägga till ny spelare</translation>
+    </message>
+</context>
+<context>
+    <name>NewGameCourses</name>
+    <message>
+        <source>new game</source>
+        <translation>nytt spel</translation>
+    </message>
+    <message>
+        <source>select course</source>
+        <translation>välj bana</translation>
+    </message>
+    <message>
+        <source>ADD COURSES</source>
+        <translation>LÄGG TILL BANOR</translation>
+    </message>
+    <message>
+        <source>Baskets:</source>
+        <translation>Korgar:</translation>
+    </message>
+    <message>
+        <source>Top-score: </source>
+        <translation>Toppresultat: </translation>
+    </message>
+    <message>
+        <source>Average-score: </source>
+        <translation>Medelresultat: </translation>
+    </message>
+    <message>
+        <source>played: </source>
+        <translation>spelat: </translation>
+    </message>
+    <message>
+        <source>No courses</source>
+        <translation>Inga banor</translation>
+    </message>
+    <message>
+        <source>pull down to add a new course</source>
+        <translation>dra neråt för att lägga till ny bana</translation>
+    </message>
+</context>
+<context>
+    <name>NewPlayer</name>
+    <message>
+        <source>SAVE</source>
+        <translation>SPARA</translation>
+    </message>
+    <message>
+        <source>Nick</source>
+        <translation>Smeknamn</translation>
+    </message>
+    <message>
+        <source>First name</source>
+        <translation>Förnamn</translation>
+    </message>
+    <message>
+        <source>Last name</source>
+        <translation>Efternamn</translation>
+    </message>
+    <message>
+        <source>Email</source>
+        <translation>E-post</translation>
+    </message>
+    <message>
+        <source>PDGA Nr</source>
+        <translation>PDGA Nr</translation>
+    </message>
+    <message>
+        <source>Notes</source>
+        <translation>Anteckningar</translation>
+    </message>
+</context>
+<context>
+    <name>Players</name>
+    <message>
+        <source>Players</source>
+        <translation>Spelare</translation>
+    </message>
+    <message>
+        <source>DELETE ALL PLAYERS</source>
+        <translation>TA BORT ALLA SPELARE</translation>
+    </message>
+    <message>
+        <source>ADD PLAYER</source>
+        <translation>LÄGG TILL SPELARE</translation>
+    </message>
+    <message>
+        <source>Played rounds: </source>
+        <translation>Spelade rundor: </translation>
+    </message>
+    <message>
+        <source>Time played: </source>
+        <translation>Spelad tid: </translation>
+    </message>
+    <message>
+        <source>Average par: </source>
+        <translation>Medelpar: </translation>
+    </message>
+    <message>
+        <source>No players</source>
+        <translation>Inga spelare</translation>
+    </message>
+    <message>
+        <source>pull down to add a new player</source>
+        <translation>dra neråt för att lägga till ny spelare</translation>
+    </message>
+</context>
+<context>
+    <name>Round</name>
+    <message>
+        <source>CANCEL ROUND</source>
+        <translation>AVBRYT RUNDAN</translation>
+    </message>
+    <message>
+        <source>|BASKET </source>
+        <translation>|KORG </translation>
+    </message>
+    <message>
+        <source>|Av.: </source>
+        <translation>|Medel: </translation>
+    </message>
+    <message>
+        <source>Best: </source>
+        <translation>Bäst: </translation>
+    </message>
+    <message>
+        <source>Average: </source>
+        <translation>Medel: </translation>
+    </message>
+    <message>
+        <source>Worst: </source>
+        <translation>Sämst: </translation>
+    </message>
+</context>
+<context>
+    <name>Round_Results</name>
+    <message>
+        <source>ENDRESULTS:</source>
+        <translation>SLUTRESULTAT:</translation>
+    </message>
+    <message>
+        <source>Endresult:</source>
+        <translation>Slutresultat:</translation>
+    </message>
+    <message>
+        <source>Time: </source>
+        <translation>Tid: </translation>
+    </message>
+    <message>
+        <source>Top- score: </source>
+        <translation>Toppresultat: </translation>
+    </message>
+    <message>
+        <source>Average-score: </source>
+        <translation>Medelresultat: </translation>
+    </message>
+    <message>
+        <source>Number-played: </source>
+        <translation>Antal spelade: </translation>
+    </message>
+</context>
+<context>
+    <name>Round_Scorecard</name>
+    <message>
+        <source>CANCEL ROUND</source>
+        <translation>AVBRYT RUNDAN</translation>
+    </message>
+    <message>
+        <source>SAVE ROUND</source>
+        <translation>SPARA RUNDAN</translation>
+    </message>
+    <message>
+        <source>player </source>
+        <translation>spelare </translation>
+    </message>
+    <message>
+        <source>total </source>
+        <translation>totalt </translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>settings</source>
+        <translation>inställningar</translation>
+    </message>
+    <message>
+        <source>look &amp; feel</source>
+        <translation>utseende &amp; känsla</translation>
+    </message>
+    <message>
+        <source>SOUND FEEDBACK</source>
+        <translation>LJUDÅTERKOPPLING</translation>
+    </message>
+    <message>
+        <source>ENABLE/DISABLE ACOUSTIC FEEDBACK WHEN PRESSING [-][+] BUTTONS</source>
+        <translation>AKTIVERA/INAKTIVERA AUKUSTISK ÅTERKOPPLING VID TRYCK PÅ [-][+]</translation>
+    </message>
+    <message>
+        <source>ABOUT</source>
+        <translation>OM</translation>
+    </message>
+    <message>
+        <source>DELETE EVERYTHING</source>
+        <translation>TA BORT ALLT</translation>
+    </message>
+    <message>
+        <source>TRANSPARENCY</source>
+        <translation>TRANSPARENS</translation>
+    </message>
+    <message>
+        <source>scorecards</source>
+        <translation>poängkort</translation>
+    </message>
+    <message>
+        <source>NICK NAME IN SCORECARDS</source>
+        <translation>SMEKNAMN PÅ POÄNGKORT</translation>
+    </message>
+    <message>
+        <source>FULL NAME IN SCORECARDS</source>
+        <translation>FULLSTÄNDIGT NAMN PÅ POÄNGKORT</translation>
+    </message>
+</context>
+<context>
+    <name>Statistics</name>
+    <message>
+        <source>scorecards</source>
+        <translation>poängkort</translation>
+    </message>
+    <message>
+        <source>sorted by date</source>
+        <translation>sorterat efter datum</translation>
+    </message>
+</context>
+</TS>
